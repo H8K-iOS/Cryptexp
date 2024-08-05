cea44a3f12009f69f5d3f33ea7410d4bf304ed2d
H8K-iOS <oleksandr.alimov18@gmail.com>
2024-08-05 08:00:36 +0200

    Initial Commit

---
+++ Cryptexp/CryptexpApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptexpApp.swift
+//  Cryptexp
+//
+//  Created by Oleksandr Alimov on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptexpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
